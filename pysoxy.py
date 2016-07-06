02893130d155b46cbf8086ba5f316c0cd1957000
misterDaneel <misterdaneel@gmail.com>
2016-07-06 22:07:36 +0200

    Pimp my code

--- pysoxy.py
+++ pysoxy.py
@@ -3,6 +3,7 @@
 # Small Socks5 Proxy Server in Python (2.7)
 # from https://github.com/MisterDaneel/
 #
+
 # Network
 import socket
 import select
@@ -13,6 +14,7 @@ import threading
 from threading import Thread
 from time import sleep
 from sys import exit
+
 #
 # Configuration
 #
@@ -21,124 +23,128 @@ BUFSIZE         = 2048
 TIMEOUT_SOCKET  = 5
 LOCAL_ADDR      = '0.0.0.0'
 LOCAL_PORT      = 5555
+
 #
 # Constants
 #
 '''Version of the protocol'''
-VER = '\x05'
+# PROTOCOL VERSION 5
+VER             = '\x05'
 '''Method constants'''
-#  '00' NO AUTHENTICATION REQUIRED
-M_NOAUTH          = '\x00'
-#  '02' USERNAME/PASSWORD
-M_AUTH            = '\x02'
-#  'FF' NO ACCEPTABLE METHODS
-M_NOTAVAILABLE    = '\xff'
+# '00' NO AUTHENTICATION REQUIRED
+M_NOAUTH        = '\x00'
+# 'FF' NO ACCEPTABLE METHODS
+M_NOTAVAILABLE  = '\xff'
 '''Command constants'''
-#  CONNECT '01'
-CMD_CONNECT         = '\x01'
-#  BIND '02'
-CMD_BIND            = '\x02'
-#  UDP ASSOCIATE '03'
-CMD_UDP_ASSOCIATIVE = '\x03'
+# CONNECT '01'
+CMD_CONNECT     = '\x01'
 '''Address type constants'''
-#  IP V4 address '01'
-ATYP_IPV4           = '\x01'
-#  DOMAINNAME '03'
-ATYP_DOMAINNAME     = '\x03'
-#  IP V6 address '04'
-ATYP_IPV6           = '\x04'
+# IP V4 address '01'
+ATYP_IPV4       = '\x01'
+# DOMAINNAME '03'
+ATYP_DOMAINNAME = '\x03'
+
 #
 # Proxy Loop
 #
-def proxy_loop(socketSrc,socketDst):
+def ProxyLoop(socket_src, socket_dst):
    while(1):
       try:
-         reader, _, _ = select.select([socketSrc, socketDst], [], [], 1)
+         reader, _, _ = select.select([socket_src, socket_dst], [], [], 1)
       except select.error:
          return
-      if not reader: return
+      if not reader:
+         return
       for socket in reader:
          data = socket.recv(BUFSIZE)
-         if not data: return
-         if socket is socketDst: socketSrc.send(data)
-         else: socketDst.send(data)
+         if not data:
+            return
+         if socket is socket_dst:
+            socket_src.send(data)
+         else:
+            socket_dst.send(data)
+   # end while
+
 #
 # Make connection to the destination host
 #
-def connectToDst(DST_ADDR, DST_PORT):
+def ConnectToDst(dst_addr, dst_port):
    try:
       s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       s.settimeout(TIMEOUT_SOCKET)
-      s.connect((DST_ADDR,DST_PORT))
+      s.connect((dst_addr,dst_port))
       return s
    except socket.error, e:
       print 'Failed to create socket - Code: ' + str(e[0]) + ', Message: ' + e[1]
       return 0
+
 #
 # Request details
 #
-def request(socketSrc):
+def Request(socket_src):
    try:
       REP = '\x07'
+      # Client Request
       #+----+-----+-------+------+----------+----------+
       #|VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
       #+----+-----+-------+------+----------+----------+
-      s5Request = socketSrc.recv(BUFSIZE)
-      # check VER, CMD and RSV
-      if (s5Request[0] != VER or s5Request[1] != CMD_CONNECT or s5Request[2] != '\x00'):
+      s5_request = socket_src.recv(BUFSIZE)
+      # Check VER, CMD and RSV
+      if (s5_request[0] != VER or s5_request[1] != CMD_CONNECT or s5_request[2] != '\x00'):
          return False
       # IPV4
-      if s5Request[3] == ATYP_IPV4:
-         DST_ADDR = '.'.join(str(ord(i)) for i in s5Request[4:-2])
-         DST_PORT = unpack('>H', s5Request[8:])[0]
-         print 'DST:', DST_ADDR, DST_PORT
-         socketDst = connectToDst(DST_ADDR, DST_PORT)
-         REP = '\x01' if socketDst == 0 else '\x00'
+      if s5_request[3] == ATYP_IPV4:
+         dst_addr = '.'.join(str(ord(i)) for i in s5_request[4:-2])
+         dst_port = unpack('>H', s5_request[8:])[0]
+         print 'DST:', dst_addr, dst_port
+         socket_dst = ConnectToDst(dst_addr, dst_port)
+         REP = '\x01' if socket_dst == 0 else '\x00'
       # DOMAINNAME
-      if s5Request[3] == ATYP_DOMAINNAME:
-         SZ_DOMAINNAME = ord(s5Request[4])
-         DOMAINNAME = s5Request[5:SZ_DOMAINNAME+5-len(s5Request)] if SZ_DOMAINNAME+5-len(s5Request) < 0 else s5Request[5:]
+      if s5_request[3] == ATYP_DOMAINNAME:
+         SZ_DOMAINNAME = ord(s5_request[4])
+         DOMAINNAME = s5_request[5:SZ_DOMAINNAME+5-len(s5_request)] if SZ_DOMAINNAME+5-len(s5_request) < 0 else s5_request[5:]
          print 'DOMAINNAME', repr(DOMAINNAME)
          return False
-      # reply
+      # Server Reply
       #+----+-----+-------+------+----------+----------+
       #|VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
       #+----+-----+-------+------+----------+----------+
-      reply = VER+REP+'\x00'+s5Request[3]+'\x00'+'\x00'+'\x00'+'\x00'+'\x00'+'\x00'
-      socketSrc.sendall(reply)
+      reply = VER+REP+'\x00'+s5_request[3]+'\x00'+'\x00'+'\x00'+'\x00'+'\x00'+'\x00'
+      socket_src.sendall(reply)
       # start proxy
-      proxy_loop(socketSrc,socketDst)
-      socketSrc.close()
-      socketDst.close()
+      ProxyLoop(socket_src,socket_dst)
+      socket_src.close()
+      socket_dst.close()
    except Exception,e:
       print 'request:', str(e)
       return False
+
 #
 # Subnegotiation
 #
-def subnegotiation(wrapper):
+def Subnegotiation(wrapper):
    try:
       print 'SOCKS request'
       res = False
-      # Version identifier/method selection message
+      # Client Version identifier/method selection message
       #+----+----------+----------+
       #|VER | NMETHODS | METHODS  |
       #+----+----------+----------+
-      IdentificationPacket = wrapper.recv(BUFSIZE)
+      identification_packet = wrapper.recv(BUFSIZE)
       # VER field
-      if (VER != IdentificationPacket[0]):
+      if (VER != identification_packet[0]):
          return res
       # METHODS fields
-      NMETHODS = ord(IdentificationPacket[1])
-      METHODS = IdentificationPacket[2:]
-      if  (len(METHODS) != NMETHODS):
+      NMETHODS = ord(identification_packet[1])
+      METHODS = identification_packet[2:]
+      if (len(METHODS) != NMETHODS):
          return res 
       for METHOD in METHODS:
          if(METHOD == M_NOAUTH):
             break
       if(METHOD != M_NOAUTH and METHOD != M_AUTH): METHOD = M_NOTAVAILABLE
       else: res = True
-      # METHOD selection message
+      # Server Method selection message
       #+----+--------+
       #|VER | METHOD |
       #+----+--------+
@@ -148,10 +154,11 @@ def subnegotiation(wrapper):
    except Exception,e:
       print 'handshake:', str(e)
       return False
+
 #
 # Create socket
 #
-def createSocket():
+def CreateSocket():
       try:
          print 'Create Socket'
          s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -177,36 +184,38 @@ def createSocket():
          s.close()
          return 0
       return s
+
 #
 # Exit
 #
-def exit_handler(signal, frame):
+def ExitHandler(signal, frame):
    exit(0)
+
 #
 # Main
 #
-newSocket = createSocket()
-if not newSocket:
+new_socket = CreateSocket()
+if not new_socket:
    print "Failed to create server"
    exit(0)
-signal(SIGINT, exit_handler)
-signal(SIGTERM, exit_handler)
+signal(SIGINT, ExitHandler)
+signal(SIGTERM, ExitHandler)
 while(1):
    if threading.activeCount() < MAX_THREADS:
       # accept
       try:
-         wrapper, addr = newSocket.accept()
+         wrapper, addr = new_socket.accept()
          wrapper.setblocking(1)
       except Exception,e:
          continue
       # thread incoming connection
-      def connection(wrapper):
-            if subnegotiation(wrapper):
-               request(wrapper)
-      recvThread = Thread(target=connection, args=(wrapper, ))
-      recvThread.start()
+      def Connection(wrapper):
+            if Subnegotiation(wrapper):
+               Request(wrapper)
+      recv_thread = Thread(target=Connection, args=(wrapper, ))
+      recv_thread.start()
    else:
       sleep(3)
-   ## end while
+   # end while
 wrapper.close()
-newSocket.close()
+new_socket.close()
