dd5a2924dbcce6bfe062d153b96b29df5f3f7ab0
misterDaneel <misterdaneel@gmail.com>
2016-07-07 20:52:19 +0200

    DNS

--- pysoxy.py
+++ pysoxy.py
@@ -5,9 +5,10 @@
 #
 
 # Network
+import ssl
 import socket
 import select
-from struct import unpack
+from struct import pack, unpack
 # System
 from signal import signal, SIGINT, SIGTERM
 import threading
@@ -18,6 +19,9 @@ from sys import exit
 #
 # Configuration
 #
+TUNNEL          = False
+SSL_SERVER_ADDR = ''
+SSL_SERVER_PORT = 0
 MAX_THREADS     = 50
 BUFSIZE         = 2048
 TIMEOUT_SOCKET  = 5
@@ -45,9 +49,17 @@ ATYP_IPV4       = '\x01'
 ATYP_DOMAINNAME = '\x03'
 
 #
+# Error
+#
+def Error():
+   import sys
+   exc_type, _, exc_tb = sys.exc_info()
+   print exc_type, exc_tb.tb_lineno
+
+#
 # Proxy Loop
 #
-def ProxyLoop(socket_src, socket_dst):
+def Proxy_Loop(socket_src, socket_dst):
    while(1):
       try:
          reader, _, _ = select.select([socket_src, socket_dst], [], [], 1)
@@ -55,27 +67,66 @@ def ProxyLoop(socket_src, socket_dst):
          return
       if not reader:
          return
-      for socket in reader:
-         data = socket.recv(BUFSIZE)
+      try:
+         for sock in reader:
+            data = sock.recv(BUFSIZE)
+            if not data:
+               continue
+            if sock is socket_dst:
+               socket_src.send(data)
+            else:
+               socket_dst.send(data)
+      except socket.error, e:
+         print 'Loop failed - Code: ' + str(e[0]) + ', Message: ' + e[1]
+         return 0
+   # end while
+
+#
+# Tunnel Loop
+#
+def Tunnel_Loop(socket_src, socket_ssl, ):
+   while(1):
+      try:
+         reader, _, _ = select.select([socket_src, socket_dst], [], [], 1)
+      except select.error:
+         return
+      if not reader:
+         return
+      for sock in reader:
+         data = sock.recv(BUFSIZE)
          if not data:
             return
-         if socket is socket_dst:
+         if sock is socket_dst:
             socket_src.send(data)
          else:
             socket_dst.send(data)
    # end while
-
+  
 #
 # Make connection to the destination host
 #
-def ConnectToDst(dst_addr, dst_port):
+def Connect_To_Dst(dst_addr, dst_port):
    try:
-      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      s.settimeout(TIMEOUT_SOCKET)
+      s = Create_Socket()
       s.connect((dst_addr,dst_port))
       return s
    except socket.error, e:
-      print 'Failed to create socket - Code: ' + str(e[0]) + ', Message: ' + e[1]
+      print 'Failed to connect to DST - Code: ' + str(e[0]) + ', Message: ' + e[1]
+      return 0
+   except:
+      Error()
+      return 0
+
+#
+# Make connection to the SSL Server
+#
+def Connect_To_SSL_Server(s):
+   try:
+      ssl_socket = ssl.wrap_socket(s, ssl_version=ssl.PROTOCOL_TLSv1)
+      ssl_socket.connect((SSL_SERVER_ADDR, SSL_SERVER_PORT))
+      return ssl_socket
+   except socket.error, e:
+      print 'Failed to connect to SSL server - Code: ' + str(e[0]) + ', Message: ' + e[1]
       return 0
 
 #
@@ -83,7 +134,6 @@ def ConnectToDst(dst_addr, dst_port):
 #
 def Request(socket_src):
    try:
-      REP = '\x07'
       # Client Request
       #+----+-----+-------+------+----------+----------+
       #|VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
@@ -94,29 +144,62 @@ def Request(socket_src):
          return False
       # IPV4
       if s5_request[3] == ATYP_IPV4:
-         dst_addr = '.'.join(str(ord(i)) for i in s5_request[4:-2])
-         dst_port = unpack('>H', s5_request[8:])[0]
-         print 'DST:', dst_addr, dst_port
-         socket_dst = ConnectToDst(dst_addr, dst_port)
-         REP = '\x01' if socket_dst == 0 else '\x00'
-      # DOMAINNAME
+         dst_addr = socket.inet_ntoa(s5_request[4:-2])#'.'.join(str(ord(i)) for i in s5_request[4:-2])
+         dst_port = unpack('>H', s5_request[8:len(s5_request)])[0]
+      # DOMAIN NAME
       if s5_request[3] == ATYP_DOMAINNAME:
-         SZ_DOMAINNAME = ord(s5_request[4])
-         DOMAINNAME = s5_request[5:SZ_DOMAINNAME+5-len(s5_request)] if SZ_DOMAINNAME+5-len(s5_request) < 0 else s5_request[5:]
-         print 'DOMAINNAME', repr(DOMAINNAME)
-         return False
+         sz_domain_name = ord(s5_request[4])
+         dst_addr = s5_request[5: 5 + sz_domain_name - len(s5_request)]
+         dst_port = unpack('>H', s5_request[5 + sz_domain_name:len(s5_request)])[0]
+      print 'DST:', dst_addr, dst_port
+   except:
+      if socket_src != 0:
+         socket_src.close()
+      Error()
+      return False
+   try:
       # Server Reply
       #+----+-----+-------+------+----------+----------+
       #|VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
       #+----+-----+-------+------+----------+----------+
-      reply = VER+REP+'\x00'+s5_request[3]+'\x00'+'\x00'+'\x00'+'\x00'+'\x00'+'\x00'
+      REP = '\x07'
+      BND = '\x00' + '\x00' + '\x00' + '\x00' + '\x00' + '\x00'
+      # SSL TUNNEL
+      if TUNNEL:
+         remote_server = Create_Socket()
+         ssl_socket = Connect_To_Ssl_Server(remote_server)
+         if ssl_socket == 0:
+            REP = '\x01'
+         else:
+            REP = '\x00'
+            BND = socket.inet_aton(remote_server.getsockname()[0])
+            BND += pack(">H", remote_server.getsockname()[1])
+      # SOCKS PROXY
+      else:
+         socket_dst = Connect_To_Dst(dst_addr, dst_port)
+         if socket_dst == 0:
+            REP = '\x01'
+         else:
+            REP = '\x00'
+            BND = socket.inet_aton(socket_dst.getsockname()[0])
+            BND += pack(">H", socket_dst.getsockname()[1])
+      reply = VER + REP + '\x00' + ATYP_IPV4
+      reply += BND
       socket_src.sendall(reply)
+      # start tunnel
+      if TUNNEL and REP == '\x00':
+         Tunnel_Loop(socket_src, ssl_socket, addr, port[0])
       # start proxy
-      ProxyLoop(socket_src,socket_dst)
-      socket_src.close()
-      socket_dst.close()
-   except Exception,e:
-      print 'request:', str(e)
+      elif REP == '\x00':
+         Proxy_Loop(socket_src, socket_dst)
+      if socket_src != 0:
+         socket_src.close()
+      if socket_dst != 0:
+         socket_dst.close()
+   except:
+      if socket_src != 0:
+         socket_src.close()
+      Error()
       return False
 
 #
@@ -124,7 +207,6 @@ def Request(socket_src):
 #
 def Subnegotiation(wrapper):
    try:
-      print 'SOCKS request'
       res = False
       # Client Version identifier/method selection message
       #+----+----------+----------+
@@ -136,51 +218,58 @@ def Subnegotiation(wrapper):
          return res
       # METHODS fields
       NMETHODS = ord(identification_packet[1])
-      METHODS = identification_packet[2:]
+      METHODS = identification_packet[2: ]
       if (len(METHODS) != NMETHODS):
          return res 
       for METHOD in METHODS:
          if(METHOD == M_NOAUTH):
             break
-      if(METHOD != M_NOAUTH and METHOD != M_AUTH): METHOD = M_NOTAVAILABLE
-      else: res = True
+      if (METHOD != M_NOAUTH and METHOD != M_AUTH):
+	     METHOD = M_NOTAVAILABLE
+      else:
+	     res = True
       # Server Method selection message
       #+----+--------+
       #|VER | METHOD |
       #+----+--------+
-      reply = VER+METHOD
+      reply = VER + METHOD
       wrapper.sendall(reply)
       return res
-   except Exception,e:
-      print 'handshake:', str(e)
+   except:
+      Error()
       return False
 
 #
 # Create socket
 #
-def CreateSocket():
+def Create_Socket():
       try:
-         print 'Create Socket'
          s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
          s.settimeout(TIMEOUT_SOCKET)
       except socket.error, e:
          print 'Failed to create socket - Code: ' + str(e[0]) + ', Message: ' + e[1]
          return 0
-      # bind
+      return s
+
+#
+# Bind_Port
+#
+def Bind_Port(s):
+      # Bind
       try:
          print 'Bind', str(LOCAL_PORT)
+         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
          s.bind((LOCAL_ADDR,LOCAL_PORT))
       except socket.error , e:
          print 'Bind failed in server - Code: ' + str(e[0]) + ', Message: ' + e[1]
          s.close()
          return 0
-      # listen
+      # Listen
       try:
          print "Listen"
          s.listen(10)
       except socket.error, e:
-         print "Listen failed - Code: "  + str(e[0]) + ", Message: " + e[1]
+         print "Listen failed - Code: " + str(e[0]) + ", Message: " + e[1]
          s.close()
          return 0
       return s
@@ -188,34 +277,36 @@ def CreateSocket():
 #
 # Exit
 #
-def ExitHandler(signal, frame):
+def Exit_Handler(signal, frame):
    exit(0)
 
 #
 # Main
 #
-new_socket = CreateSocket()
-if not new_socket:
-   print "Failed to create server"
-   exit(0)
-signal(SIGINT, ExitHandler)
-signal(SIGTERM, ExitHandler)
-while(1):
-   if threading.activeCount() < MAX_THREADS:
-      # accept
-      try:
-         wrapper, addr = new_socket.accept()
-         wrapper.setblocking(1)
-      except Exception,e:
-         continue
-      # thread incoming connection
-      def Connection(wrapper):
-            if Subnegotiation(wrapper):
-               Request(wrapper)
-      recv_thread = Thread(target=Connection, args=(wrapper, ))
-      recv_thread.start()
-   else:
-      sleep(3)
-   # end while
-wrapper.close()
-new_socket.close()
+if __name__ == '__main__':
+	new_socket = Create_Socket()
+	Bind_Port(new_socket)
+	if not new_socket:
+	   print "Failed to create server"
+	   exit(0)
+	signal(SIGINT, Exit_Handler)
+	signal(SIGTERM, Exit_Handler)
+	while(1):
+	   if threading.activeCount() < MAX_THREADS:
+		  # Accept
+		  try:
+			 wrapper, addr = new_socket.accept()
+			 wrapper.setblocking(1)
+		  except:
+			 continue
+		  # Thread incoming connection
+		  def Connection(wrapper):
+				if Subnegotiation(wrapper):
+				   Request(wrapper)
+		  recv_thread = Thread(target=Connection, args=(wrapper, ))
+		  recv_thread.start()
+	   else:
+		  sleep(3)
+	   # end while
+	wrapper.close()
+	new_socket.close()
