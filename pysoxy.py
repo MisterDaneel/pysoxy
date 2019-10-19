33d61154f7156f813cae751f0b4c5d7ad1b7ea36
MisterDaneel <misterdaneel@gmail.com>
2019-10-19 10:15:00 +0200

    Add some explanations

--- pysoxy.py
+++ pysoxy.py
@@ -68,7 +68,41 @@ class ExitStatus:
 
 
 class Request(LoggerMixin):
-    """_____<here needs to come a one/multi line summary>_____."""
+    """
+        Once the method-dependent subnegotiation has completed, the client
+        sends the request details.  If the negotiated method includes
+        encapsulation for purposes of integrity checking and/or
+        confidentiality, these requests MUST be encapsulated in the method-
+        dependent encapsulation.
+
+        The SOCKS request is formed as follows:
+
+             +----+-----+-------+------+----------+----------+
+             |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+             +----+-----+-------+------+----------+----------+
+             | 1  |  1  | X'00' |  1   | Variable |    2     |
+             +----+-----+-------+------+----------+----------+
+
+          Where:
+
+               o  VER    protocol version: X'05'
+               o  CMD
+                  o  CONNECT X'01'
+                  o  BIND X'02'
+                  o  UDP ASSOCIATE X'03'
+               o  RSV    RESERVED
+               o  ATYP   address type of following address
+                  o  IP V4 address: X'01'
+                  o  DOMAINNAME: X'03'
+                  o  IP V6 address: X'04'
+               o  DST.ADDR       desired destination address
+               o  DST.PORT desired destination port in network octet
+                  order
+
+        The SOCKS server will typically evaluate the request based on source
+        and destination addresses, and return one or more reply messages, as
+        appropriate for the request type.
+    """ 
 
     def __init__(self,
                  wrapper,
@@ -81,7 +115,11 @@ class Request(LoggerMixin):
         self.socket_dst = None
 
     def proxy_loop(self):
-        """______<here needs to come a one/multi line summary>______"""
+        """
+            The select function blocks the thread until data is available
+            on a specified socket
+            Then the data is forwarded to the right recipient.
+        """
         while not EXIT.get_status():
             try:
                 reader, _, _ = select.select([self.wrapper, self.socket_dst], [], [], 1)
@@ -176,7 +214,43 @@ class Request(LoggerMixin):
 
 
 class Subnegotiation(LoggerMixin):
-    """_____<here needs to come a one/multi line summary>_____."""
+    """
+        The client connects to the server, and sends a version
+        identifier/method selection message:
+
+                        +----+----------+----------+
+                        |VER | NMETHODS | METHODS  |
+                        +----+----------+----------+
+                        | 1  |    1     | 1 to 255 |
+                        +----+----------+----------+
+
+        The VER field is set to X'05' for this version of the protocol.  The
+        NMETHODS field contains the number of method identifier octets that
+        appear in the METHODS field.
+
+        The server selects from one of the methods given in METHODS, and
+        sends a METHOD selection message:
+
+                              +----+--------+
+                              |VER | METHOD |
+                              +----+--------+
+                              | 1  |   1    |
+                              +----+--------+
+
+        If the selected METHOD is X'FF', none of the methods listed by the
+        client are acceptable, and the client MUST close the connection.
+
+        The values currently defined for METHOD are:
+
+               o  X'00' NO AUTHENTICATION REQUIRED
+               o  X'01' GSSAPI
+               o  X'02' USERNAME/PASSWORD
+               o  X'03' to X'7F' IANA ASSIGNED
+               o  X'80' to X'FE' RESERVED FOR PRIVATE METHODS
+               o  X'FF' NO ACCEPTABLE METHODS
+
+        The client and server then enter a method-specific sub-negotiation.
+    """
 
     def __init__(self, wrapper):
         """Initializes a Subnegotiation object."""
@@ -366,6 +440,9 @@ class Proxy(LoggerMixin):
                 conn.setblocking(True)  # 1 == True and 0 == False
             except socket.timeout as e:
                 # @Daneel, could you please explain why this exception happens and how this can be mitigated?
+                # This exception happens because the socket timeout after TIMEOUT_SOCKET seconds (in script header)
+                # Without this timeout, the program will be stuck on accept until a connection happens
+                # and cannot manage an EXIT signal (while condition)
                 continue
             recv_thread = threading.Thread(target=connection, args=(conn, self.local_addr_e))
             recv_thread.start()
